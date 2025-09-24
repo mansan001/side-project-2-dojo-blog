5693e874a79424369731d71cbf421502b33a321c
lilangulap <131538049+lilangulap@users.noreply.github.com>
2025-09-24 20:41:54 +0800

    Migrated from array to json

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,21 +1,19 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => res.json()) // transform the data into json, res means response.
+            .then(data => setBlogs(data)) // set the data
+    }, []);
+ 
     return (
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p> Written By: { blog.author }</p>
-                </div>
-            ))}
+            {blogs && <BlogList blogs={ blogs }/>}
         </div>
     );
 }
